4b350614c28225225237619d283434d0bc95bfd9
williamlow <44437051+williamlow@users.noreply.github.com>
2021-11-17 16:08:48 +0000

    Rename pointData.js to example/pointData.js

---
+++ example/pointData.js
@@ -0,0 +1,17 @@
+var pointData = {"type":"FeatureCollection","features":[
+{"type":"Feature","properties":{},"geometry":{"type":"Point","coordinates":[-0.16513824462890625,51.54686881000932]}},
+{"type":"Feature","properties":{},"geometry":{"type":"Point","coordinates":[-0.16513824462890625,51.46416788407397]}},
+{"type":"Feature","properties":{},"geometry":{"type":"Point","coordinates":[-0.1153564453125,51.527329038465936]}},
+{"type":"Feature","properties":{},"geometry":{"type":"Point","coordinates":[-0.06591796875,51.539822646414315]}},
+{"type":"Feature","properties":{},"geometry":{"type":"Point","coordinates":[-0.22659301757812497,51.56277204635416]}},
+{"type":"Feature","properties":{},"geometry":{"type":"Point","coordinates":[-0.12239456176757812,51.5738689970858]}},
+{"type":"Feature","properties":{},"geometry":{"type":"Point","coordinates":[-0.01476287841796875,51.4968815398868]}},
+{"type":"Feature","properties":{},"geometry":{"type":"Point","coordinates":[-0.24650573730468747,51.53587204710932]}},
+{"type":"Feature","properties":{},"geometry":{"type":"Point","coordinates":[-0.00274658203125,51.578776399817066]}},
+{"type":"Feature","properties":{},"geometry":{"type":"Point","coordinates":[-0.26092529296875,51.45079771145382]}},
+{"type":"Feature","properties":{},"geometry":{"type":"Point","coordinates":[0.1627349853515625,51.698097536240525]}},
+{"type":"Feature","properties":{},"geometry":{"type":"Point","coordinates":[0.1270294189453125,51.37049441273878]}},
+{"type":"Feature","properties":{},"geometry":{"type":"Point","coordinates":[-0.35980224609374994,51.32203016092245]}},
+{"type":"Feature","properties":{},"geometry":{"type":"Point","coordinates":[-0.0501251220703125,51.411199044550045]}},
+{"type":"Feature","properties":{},"geometry":{"type":"Point","coordinates":[-0.3412628173828125,51.59754765771458]}}]
+}
